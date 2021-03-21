321aae0fd790da78e102602d8738cafde437339b
Chris Roberts <cpr420@gmail.com>
2021-03-21 16:27:36 -0600

    PrefsViewState.cpp: swtich to Haiku native i18n

--- Source/Beezer/PrefsViewState.cpp
+++ Source/Beezer/PrefsViewState.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2009, Ramshankar (aka Teknomancer)
- * Copyright (c) 2011, Chris Roberts
+ * Copyright (c) 2011-2021, Chris Roberts
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -33,7 +33,6 @@
 
 #include "AppConstants.h"
 #include "BitmapPool.h"
-#include "LangStrings.h"
 #include "LocalUtils.h"
 #include "Preferences.h"
 #include "PrefsFields.h"
@@ -41,9 +40,18 @@
 #include "UIConstants.h"
 
 
+#ifdef HAIKU_ENABLE_I18N
+#include <Catalog.h>
+
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "PrefsViewState"
+#else
+#define B_TRANSLATE(x) x
+#endif
+
 
 PrefsViewState::PrefsViewState(BRect frame)
-    : PrefsView(frame, str(S_PREFS_TITLE_STATE), str(S_PREFS_DESC_STATE))
+    : PrefsView(frame, B_TRANSLATE("State"), B_TRANSLATE("Configure what to tag along with archives as attributes"))
 {
     SetBitmap(BitmapPool::LoadAppVector("Img:Prefs_State", 20, 20));
     Render();
@@ -54,31 +62,31 @@ PrefsViewState::PrefsViewState(BRect frame)
 void PrefsViewState::Render()
 {
     BStringView* storeStrView = new BStringView(BRect(m_margin, m_margin, 0, 0), NULL,
-            str(S_PREFS_STATE_STORE));
+            B_TRANSLATE("Store automatically"));
     storeStrView->SetFont(&m_sectionFont);
     storeStrView->ResizeToPreferred();
     storeStrView->SetLowColor(ViewColor());
 
     m_storeUIChk = new BCheckBox(BRect(3 * m_margin, storeStrView->Frame().bottom + m_vGap, 0, 0),
-                                 "PrefsViewState:storeUIChk", str(S_PREFS_STATE_UI), NULL);
+                                 "PrefsViewState:storeUIChk", B_TRANSLATE("Interface state"), NULL);
     m_storeUIChk->ResizeToPreferred();
 
     m_storeArkChk = new BCheckBox(BRect(3 * m_margin, m_storeUIChk->Frame().bottom + m_vGap, 0, 0),
-                                  "PrefsViewState:storeArkChk", str(S_PREFS_STATE_ARK), NULL);
+                                  "PrefsViewState:storeArkChk", B_TRANSLATE("Archiver settings"), NULL);
     m_storeArkChk->ResizeToPreferred();
 
     BStringView* restoreStrView = new BStringView(BRect(m_margin, m_storeArkChk->Frame().bottom + m_vGap +
-            m_storeArkChk->Frame().Height() / 2, 0, 0), NULL, str(S_PREFS_STATE_RESTORE));
+            m_storeArkChk->Frame().Height() / 2, 0, 0), NULL, B_TRANSLATE("Restore automatically"));
     restoreStrView->SetFont(&m_sectionFont);
     restoreStrView->ResizeToPreferred();
     restoreStrView->SetLowColor(ViewColor());
 
     m_restoreUIChk = new BCheckBox(BRect(3 * m_margin, restoreStrView->Frame().bottom + m_vGap, 0, 0),
-                                   "PrefsViewState:restoreUIChk", str(S_PREFS_STATE_UI), NULL);
+                                   "PrefsViewState:restoreUIChk", B_TRANSLATE("Interface state"), NULL);
     m_restoreUIChk->ResizeToPreferred();
 
     m_restoreArkChk = new BCheckBox(BRect(3 * m_margin, m_restoreUIChk->Frame().bottom + m_vGap, 0, 0),
-                                    "PrefsViewState:restoreArkChk", str(S_PREFS_STATE_ARK), NULL);
+                                    "PrefsViewState:restoreArkChk", B_TRANSLATE("Archiver settings"), NULL);
     m_restoreArkChk->ResizeToPreferred();
 
     AddChild(storeStrView);
