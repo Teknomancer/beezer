138d936cec325a18af2dbe8b0408815674ddaaf6
Chris Roberts <cpr420@gmail.com>
2021-03-21 22:19:53 -0600

    SearchWindow.cpp: update copyright year

--- Source/Beezer/SearchWindow.cpp
+++ Source/Beezer/SearchWindow.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012, Ramshankar (aka Teknomancer)
- * Copyright (c) 2011, Chris Roberts
+ * Copyright (c) 2011-2021, Chris Roberts
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
